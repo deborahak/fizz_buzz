8fcb4ff500f48618d179d631b0a347bcd34b7e8a
Deborah Kain <deborahkain@mac.com>
2016-09-03 10:36:16 -0700

    second

--- js/app.js
+++ js/app.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
     
     for (var i = 1; i <= 100; i++) {
         if (i % 15 === 0){
-         document.write("Fizz Buzz <br>");
+         $("#fizzCount").append("Fizz Buzz <br>");
          console.log("Fizz Buzz")
          }
         else if (i % 3 === 0) {
             // code block that handles divisibilty by 3
-            document.write ("Fizz <br>");
+            $("#fizzCount").append ("Fizz <br>");
             console.log("Fizz")
         } 
         else if (i % 5 === 0) {
-             document.write("Buzz <br>");
+             $("#fizzCount").append("Buzz <br>");
              console.log("Buzz")
         } 
         else {
-             document.write(i + "<br>");
+             $("#fizzCount").append (i + "<br>");
              console.log(i)
         }
         
